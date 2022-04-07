33d3fede548e945e09a507c00380be81127ec379
darmiel <71837281+darmiel@users.noreply.github.com>
2022-04-07 19:05:39 +0200

    refactor: faster meta update

--- main.go
+++ main.go
@@ -10,7 +10,6 @@ import (
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 	"os"
-	"strings"
 	"time"
 )
 
@@ -50,100 +49,26 @@ func main() {
 	}
 	log.Info("OK!")
 
-	// find all michael reeves videos
-	/*
-		res, err := service.Search.List([]string{"snippet"}).
-			ChannelId("UCtHaxi4GTYDpJgMSGy7AeSw").
-			MaxResults(50).
-			Do()
-		if err != nil {
-			log.WithError(err).Warn("cannot query videos")
-		} else {
-			for _, s := range res.Items {
-				var count int64
-				if err = db.Model(&Video{}).
-					Where("id = ?", s.Id.VideoId).
-					Count(&count).Error; err != nil {
-					log.WithError(err).Warn("cannot get count")
-				} else if count == 0 {
-					log.Infof("Inserting video %s", s.Id.VideoId)
-					if err = db.Create(&Video{ID: s.Id.VideoId, ChannelID: s.Id.ChannelId}).Error; err != nil {
-						log.WithError(err).Warn("cannot insert video")
-					}
-				}
-			}
-		}
-	*/
-
 	c := cron.New(cron.WithSeconds())
-	if _, err = c.AddFunc("*/5 * * * * *", func() {
-		log.Debug("checking...")
+	if _, err = c.AddFunc("*/10 * * * * *", func() {
+		log.Debug("[Meta-Update] Checking...")
 
 		var videos []*Video
 		if err = db.Find(&videos).Error; err != nil {
 			log.WithError(err).Warn("[Meta-Update] cannot fetch videos from database")
 			return
 		}
+
 		log.Infof("[Meta-Update] Updating %d videos...", len(videos))
 
 		swStart := time.Now()
-		for i, v := range videos {
-			metas, err := service.Videos.List(part).
-				Id(v.ID).
-				Do()
-			if err != nil {
-				log.WithError(err).Warnf("[Meta-Update] cannot get meta for video #%d: %s", i, v.ID)
-				continue
-			}
-			if len(metas.Items) < 1 {
-				log.Warnf("[Meta-Update] Cannot find video #%d %s. Is it private?", i, v.ID)
-				// TODO: private video
-				continue
-			}
-			meta := metas.Items[0]
-
-			// compare meta
-			v.Title = meta.Snippet.Title
-			v.Description = meta.Snippet.Description
-			v.ViewCount = meta.Statistics.ViewCount
-			v.LikeCount = meta.Statistics.LikeCount
-			v.CommentCount = meta.Statistics.CommentCount
-			v.Tags = strings.Join(meta.Snippet.Tags, ",")
-			// TODO: Video length
-
-			if meta.Status != nil && meta.Status.MadeForKids {
-				v.Rating = KidsRating
-			} else if meta.ContentDetails.ContentRating.YtRating == "2022-03-31T23:49:49Z" {
-				v.Rating = AgeRestrictedRating
-			} else {
-				v.Rating = NormalRating
-			}
-
-			publishedAt, err := time.Parse(time.RFC3339, meta.Snippet.PublishedAt)
-			if err != nil {
-				log.WithError(err).Warn("[Meta-Update] cannot parse time")
-			} else {
-				v.PublishedAt = &publishedAt
-			}
-
-			if meta.Status != nil {
-				privacy := meta.Status.PrivacyStatus
-				switch privacy {
-				case "public":
-					v.PrivacyStatus = PublicPrivacyStatus
-				case "unlisted":
-					v.PrivacyStatus = UnlistedPrivacyStatus
-				}
-			}
-
-			t := time.Now()
-			v.LastUpdated = &t
-
-			if err = db.Updates(v).Error; err != nil {
-				log.WithError(err).Warn("[Meta-Update] cannot update video")
-			}
+		if videos, err = updateVideos(service, db, videos); err != nil {
+			log.WithError(err).Warn("cannot update videos")
 		}
-		log.Infof("[Meta-Update] Done! Took %s", time.Now().Sub(swStart).String())
+		swStop := time.Now()
+
+		log.Infof("[Meta-Update] Done! Took %s. %d videos should be downloaded.",
+			swStop.Sub(swStart).String(), len(videos))
 	}); err != nil {
 		log.WithError(err).Fatal("Cannot start meta updater")
 		return
--- updater.go
+++ updater.go
@@ -6,87 +6,98 @@ import (
 	"google.golang.org/api/youtube/v3"
 	"gorm.io/gorm"
 	"strings"
-	"sync"
 	"time"
 )
 
-var part = []string{
+// MetaUpdateParts contains the requested parts for the
+// YouTube Data V3 API call
+var MetaUpdateParts = []string{
 	"contentDetails",
 	"id",
-	"liveStreamingDetails",
-	"localizations",
-	"player",
-	"recordingDetails",
 	"snippet",
 	"statistics",
 	"status",
-	"topicDetails",
 }
 
 var ErrVideoNotFound = errors.New("video not found")
 
-func updateVideos(service *youtube.Service, db *gorm.DB) (err error) {
-	var videos []*Video
-	if err = db.Find(&videos).Error; err != nil {
-		return
+const (
+	WorkerCount = 8
+)
+
+func updateVideos(service *youtube.Service, db *gorm.DB, videos []*Video) (dl []*Video, err error) {
+	jobsChan := make(chan *Video, len(videos))
+	resChan := make(chan *Video, len(videos))
+	for i := 0; i < WorkerCount; i++ {
+		go updateWorker(i, jobsChan, resChan, service, db)
 	}
 
-	jobsChan := make(chan *Video)
-	jobsCount := 6
-
-	var wg sync.WaitGroup
-	for i := 0; i < jobsCount; i++ {
-		wg.Add(1)
-
-		go func(n int) {
-			for {
-				select {
-				case job, more := <-jobsChan:
-					if !more {
-						wg.Done()
-						return
-					}
-					if err = updateJob(service, db, job); err != nil {
-						log.WithError(err).Warnf("[Job %d] Failed to update video", n)
-					}
-				}
-			}
-		}(i)
-	}
-
+	// distribute jobs
 	for _, v := range videos {
 		jobsChan <- v
 	}
 	close(jobsChan)
 
-	wg.Wait()
+	// await results and save them to the dl array
+	for i := 0; i < len(videos); i++ {
+		res := <-resChan
+		if res != nil {
+			dl = append(dl, res)
+		}
+	}
+	close(resChan)
+
 	return
 }
 
-func updateJob(service *youtube.Service, db *gorm.DB, v *Video) (err error) {
+func updateWorker(i int, in, out chan *Video, service *youtube.Service, db *gorm.DB) {
+	for {
+		select {
+		case job, more := <-in:
+			if !more {
+				log.Infof("[Job %d] Done!", i)
+				return
+			}
+			dl, err := updateJob(service, db, job)
+			if err != nil {
+				log.WithError(err).Warnf("[Job %d] Failed to update video", i)
+			}
+			if dl {
+				log.Infof("[Job %d] [Video %s] should be downloaded.", i, job.ID)
+				out <- job
+			} else {
+				out <- nil
+			}
+		}
+	}
+}
+
+func updateJob(service *youtube.Service, db *gorm.DB, v *Video) (dl bool, err error) {
 	var resp *youtube.VideoListResponse
-	if resp, err = service.Videos.List(part).Id(v.ID).Do(); err != nil {
+	if resp, err = service.Videos.List(MetaUpdateParts).Id(v.ID).Do(); err != nil {
 		return
 	}
 	if len(resp.Items) < 1 {
-		return ErrVideoNotFound // TODO: Video is private
+		// TODO: Video is private
+		return false, ErrVideoNotFound
 	}
-	r := resp.Items[0]
 
-	t := time.Now()
-
-	check := func(old, new, field string) error {
-		if old == new {
-			return nil
+	var (
+		r     = resp.Items[0]
+		t     = time.Now()
+		check = func(old, new, field string) error {
+			if old == new {
+				return nil
+			}
+			return db.Create(&VideoHistory{
+				VideoID:   v.ID,
+				Field:     field,
+				Old:       old,
+				New:       new,
+				UpdatedAt: t,
+			}).Error
 		}
-		return db.Create(&VideoHistory{
-			VideoID:   v.ID,
-			Field:     field,
-			Old:       old,
-			New:       new,
-			UpdatedAt: t,
-		}).Error
-	}
+	)
 
 	// title
 	if err = check(v.Title, r.Snippet.Title, "title"); err != nil {
@@ -112,8 +123,10 @@ func updateJob(service *youtube.Service, db *gorm.DB, v *Video) (err error) {
 		if err = check(v.VideoLength, det.Duration, "length"); err != nil {
 			return
 		}
+		if v.VideoLength != det.Duration {
+			dl = true
+		}
 		v.VideoLength = det.Duration
-		// TODO: mark video to download
 	}
 
 	// view count
@@ -150,5 +163,6 @@ func updateJob(service *youtube.Service, db *gorm.DB, v *Video) (err error) {
 		}
 	}
 
+	err = db.Updates(v).Error
 	return
 }
