1fe098b0c6e394716083199248be085c3787a591
TomRomeo <Tom.Romeo.Doil@gmail.com>
2022-04-09 16:22:18 +0200

    continued work on addVideo Route

--- internal/rest/routes.video.add.go
+++ internal/rest/routes.video.add.go
@@ -1,13 +1,14 @@
 package rest
 
 import (
+	"github.com/ICBX/penguin/pkg/common"
 	"github.com/gofiber/fiber/v2"
 )
 
 // rest payloads
 type newVideoPayload struct {
-	User    string `json:"user"`
-	VideoID string `json:"video"`
+	UserID  uint   `json:"userID"`
+	VideoID string `json:"videoID"`
 }
 
 func (s *Server) routeVideoAdd(ctx *fiber.Ctx) (err error) {
@@ -17,6 +18,36 @@ func (s *Server) routeVideoAdd(ctx *fiber.Ctx) (err error) {
 		return
 	}
 
-	// TODO: add new video to download queue
+	user := common.User{}
+	r := s.db.Find(&user, "ID = ?", req.UserID)
+
+	if r.RowsAffected != 1 {
+		return common.ErrUserDoesntExist
+	}
+
+	video := common.Video{
+		ID: req.VideoID,
+	}
+
+	// check if video already in database
+	r = s.db.Find(&video, "ID = ?", req.VideoID)
+	if r.RowsAffected == 1 {
+
+		// check if requesting user already added that video
+		for _, u := range video.Users {
+			if u.ID == req.UserID {
+				return common.ErrVideoAlreadyAdded
+			}
+		}
+
+		// otherwise add requesting user to userlist
+		video.Users = append(video.Users, &user)
+		s.db.Updates(video)
+	}
+
+	// otherwise add empty video to db
+	video.Users = append(video.Users, &user)
+	s.db.Create(&video)
+
 	return nil
 }
--- pkg/common/errors.go
+++ pkg/common/errors.go
@@ -3,3 +3,7 @@ package common
 import "errors"
 
 var ErrVideoNotFound = errors.New("video not found")
+
+var ErrVideoAlreadyAdded = errors.New("This video already exists in your database")
+
+var ErrUserDoesntExist = errors.New("The specified user wasn't found")
